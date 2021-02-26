2d5c9cfa7c24d510c16b2bc33b08b20a3211e885
donjin lee <ldj04120@gmail.com>
2021-02-26 09:14:15 +0900

    GUI 라이브러리 변경

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from tkinter import *
 
 import chromedriver_autoinstaller
+import wx
 import yaml
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -60,7 +61,7 @@ ampm = now.strftime('%p')
 # 현재시간이 오전이면 출근 오후면 퇴근 클릭
 
 workBtn = browser.find_element_by_id('workIn') if ampm == 'AM' else browser.find_element_by_id('workOut')
-workBtn.click()
+# workBtn.click()
 
 # 결과 메시지 박스 출력
 imgPath = resource_path('logs\\' + ampm + now.strftime('%Y%m%d_%H_%M_%S') + '.png')
@@ -68,19 +69,16 @@ print(imgPath)
 browser.get_screenshot_as_file(imgPath)
 time.sleep(2)
 
-msgBox = Tk()
-resultImg = PhotoImage(file=imgPath)
-imgLabel = Label(image=resultImg)
-imgLabel.image = resultImg
-imgLabel.pack()
-imgLabel.place(x=0, y=0)
-
 title = '출근처리 결과' if ampm == 'AM' else '퇴근처리 결과'
 
-msgBox.title(title)
-msgBox.geometry('700x500+10+10')
-msgBox.mainloop()
+app = wx.App(False)
+frame = wx.Frame(None, wx.ID_ANY, title, size=(700, 500))
+
+resultImg = wx.Image(imgPath, wx.BITMAP_TYPE_ANY).ConvertToBitmap()
+wx.StaticBitmap(frame, -1, bitmap=resultImg, pos=(10, 5), size=(700, 500))
+
+frame.Show(True)
+app.MainLoop()
 
 time.sleep(2)
-
 browser.quit()
